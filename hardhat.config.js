8459f7bee2772d1f0b0bf5fcd36a75d6cf5f8f82
Kali-Design <hm_leo@hotmail.com>
2021-07-16 00:14:58 +0200

    Finish

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,41 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-solhint');
+require('hardhat-docgen');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require('dotenv').config();
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: false,
+  },
 };
--- test/projectNft-test.js
+++ test/projectNft-test.js
@@ -2,9 +2,9 @@ const { expect } = require('chai');
 
 describe('ProjectNft', function () {
   it('Should return the new nft once it\'s changed', async function () {
-   
-    const ProjectNft= await ethers.getContractFactory('ProjectNft');
-    const projectnft= await ProjectNft.deploy('Hello, world!');
+    // eslint-disable-next-line no-undef
+    const ProjectNft = await ethers.getContractFactory('ProjectNft');
+    const projectnft = await ProjectNft.deploy('Hello, world!');
     await projectnft.deployed();
 
     expect(await projectnft.nft()).to.equal('Hello, world!');
